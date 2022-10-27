30750e088d2a781025ac3017ee29357580332c4d
dieEisdiele <86861703+dieEisdiele@users.noreply.github.com>
2022-10-27 22:24:48 +1100

    Update dice.rs

--- src/dice.rs
+++ src/dice.rs
@@ -2,7 +2,7 @@ use rand::prelude::*;
 
 pub fn sum_n_dice(n: u32, faces: u32) -> u32 {
     let mut sum: u32 = 0;
-    let mut rng: ThreadRng= rand::thread_rng();
+    let mut rng: ThreadRng = rand::thread_rng();
     
     for _ in 0..n {
         let result: u32 = rng.gen_range(1..=faces);
