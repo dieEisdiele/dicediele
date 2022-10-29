33a32bfaebbc6a7ab1799010f6f9cc07ec255827
dieEisdiele <86861703+dieEisdiele@users.noreply.github.com>
2022-10-29 11:48:45 +1100

    formatting fixes

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,6 @@ fn main() {
     println!("Ready to roll.");
 
     let dice_inputs: [u32; 2] = terminal_io::t_roll_request();
-    for input in dice_inputs {
-        println!("{}", input);
-    }
 
     let rolls: Vec<u32> = dice::roll_ndx(dice_inputs[0], dice_inputs[1]);
     for result in &rolls {
@@ -14,5 +11,5 @@ fn main() {
     }
 
     let sum: u32 = rolls.iter().sum();
-    println!("Your total is {}.", sum); 
+    println!("Your total is {}.", sum);
 }
